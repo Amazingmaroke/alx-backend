5f596025c7d48aaaf618d40853e6f7e1a2d3154e
AMAZING-MAROKE <marokemaroke2020@gmail.com>
2023-10-15 19:20:10 +0100

    all

--- 0x01-caching/pra.py
+++
@@ -1,7 +0,0 @@
-from collections import Counter
-
-my_list = ['A', 'A', 'B', 'C', 'B', 'B']
-counter = Counter(my_list)
-
-min_count = min(counter.items())
-print("Minimum count:", min_count)
--- 0x02-i18n/config.py
+++
@@ -1,8 +0,0 @@
-""" Config module for i18n"""
-
-
-class Config:
-    """Config class for i18n"""
-    LANGUAGES = ["en", "fr"]
-    BABEL_DEFAULT_LOCALE = "en"
-    BABEL_DEFAULT_TIMEZONE = "UTC"
